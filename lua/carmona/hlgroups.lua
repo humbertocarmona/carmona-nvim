63c441fb1862b819ae5a2a5d92b061e03543ba00
Humberto Carmona <carmona@fisica.ufc.br>
2022-11-14 12:18:44 -0300

    definitions and constants

--- lua/carmona/hlgroups.lua
+++ lua/carmona/hlgroups.lua
@@ -69,7 +69,7 @@ function M.setup(colors, config)
         Whitespace                        = { fg = colors.bg_light2 },
         WildMenu                          = { link = "Pmenu" },
 
-        Constant                          = { fg = colors.co },
+        Constant                          = { fg = colors.sm },
         String                            = { fg = colors.sp },
         Character                         = { link = "String" },
         Number                            = { fg = colors.nu },
@@ -87,7 +87,7 @@ function M.setup(colors, config)
         Keyword                           = { fg = colors.sp3, style = config.keywordStyle },
         Exception                         = { fg = colors.sp3 },
 
-        PreProc                           = { fg = colors.pp },
+        PreProc                           = { fg = colors.nu },
         -- Include                        = {},
         -- Define                         = {},
         -- Macro                          = {},
